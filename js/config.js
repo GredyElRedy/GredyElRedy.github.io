74683f7663ea178ab6b87b29186fc618be3b70b0
GredyElRedy <69074016+GredyElRedy@users.noreply.github.com>
2020-08-03 16:46:35 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVCBio1jBwfHhz-GTc21DkA3uO5R_JqBE",
+  authDomain: "bigotitos-de-oro.firebaseapp.com",
+  databaseURL: "https://bigotitos-de-oro.firebaseio.com",
+  projectId: "bigotitos-de-oro",
+  storageBucket: "bigotitos-de-oro.appspot.com",
+  messagingSenderId: "1017594664194",
+  appId: "1:1017594664194:web:25409bb5045a4845c29e1b",
+};
+
+firebase.initializeApp(firebaseConfig);
